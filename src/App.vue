7681dd9e0696174293d8519dcc6a92d9e982ee11
JotaPrimo <gestald118@gmail.com>
2023-06-18 13:43:07 -0400

    28. Creating new Vue.js 3 project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
+  import Navbar from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
+  import TasksPage from './components/pages/TasksPage.vue';
 </script>
 
-<template>
-  
+<template>  
+    <!-- init navbar-->
+    <Navbar />
+    <!-- end navbar-->
+
+    <!--init TasksPage-->
+    <TasksPage />
+    <!--end TasksPage-->
+
+     <!--init Footer-->
+     <Footer />
+    <!--end Footer-->
+
 </template>
 
 <style scoped>
