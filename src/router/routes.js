38b586ac25dcf13b63b41aca42f51233e489c604
JotaPrimo <gestald118@gmail.com>
2023-06-19 21:25:22 -0400

    50. Navigation Guards

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import LoginPage from "../components/pages/LoginPage.vue";
 import TasksPage from "../components/pages/TasksPage.vue";
 import RegisterPage from "../components/pages/RegisterPage.vue";
 import SummaryPage from "../components/pages/SummaryPage.vue";
+import NotFoundErrorPage from "../components/pages/errors/NotFoundErrorPage.vue";
 
 const routes = [
     {
@@ -26,6 +27,12 @@ const routes = [
         path: '/summary',
         name: 'summary',
         component: SummaryPage,
+    },
+
+    {
+        path: "/:notFound(.*)",
+        name: 'error.404',
+        component: NotFoundErrorPage,
     },
 
 ]
