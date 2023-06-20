ac1ae06a2a4b4b32c39f1e928654a66f8ad62f6e
JotaPrimo <gestald118@gmail.com>
2023-06-19 21:45:54 -0400

    52. Transitions

--- src/components/pages/LoginPage.vue
+++ src/components/pages/LoginPage.vue
@@ -1,12 +1,14 @@
 <template>
-    <h1>Login Page</h1>
-    <butto class="btn btn-primary" @click="handleClick">Login</butto>
+    <main style="min-height: 50vh; margin-top: 2rem;">
+        <h1>Login Page</h1>
+        <button class="btn btn-primary" @click="handleClick">Login</button>
+    </main>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
 
-const router =  useRouter();
+const router = useRouter();
 const handleClick = () => {
     router.push({ name: 'tasks' })
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,44 +3,52 @@ import TasksPage from "../components/pages/TasksPage.vue";
 import RegisterPage from "../components/pages/RegisterPage.vue";
 import SummaryPage from "../components/pages/SummaryPage.vue";
 import NotFoundErrorPage from "../components/pages/errors/NotFoundErrorPage.vue";
+import HomePage from "../components/pages/HomePage.vue";
 
 const routes = [
     {
-        path: '/tasks',
-        name: 'tasks',
-        component: TasksPage,
-        meta: {
-            auth: true
-        }
+      path: "/",
+      component: HomePage,
+      name: "home",
     },
-
     {
-        path: '/login',
-        name: 'login',
-        component: LoginPage,
+      path: "/tasks",
+      component: TasksPage,
+      name: "tasks",
+      meta: {
+        auth: true,
+      },
     },
-
     {
-        path: '/register',
-        name: 'register',
-        component: RegisterPage,
+      path: "/summary",
+      component: SummaryPage,
+      name: "summary",
+      meta: {
+        auth: true,
+      },
     },
-
     {
-        path: '/summary',
-        name: 'summary',
-        component: SummaryPage,
-        meta: {
-            auth: true
-        }
+      path: "/login",
+      component: LoginPage,
+      name: "login",
+      meta: {
+        guest: true,
+      },
     },
-
     {
-        path: "/:notFound(.*)",
-        name: 'error.404',
-        component: NotFoundErrorPage,
+      path: "/register",
+      component: RegisterPage,
+      name: "register",
+      meta: {
+        guest: true,
+      },
     },
-
-]
+    {
+      path: "/:notFound(.*)",
+      name: "error.404",
+      component: NotFoundErrorPage,
+    },
+  ];
+  
 
 export default routes
